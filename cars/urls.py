ad4947d6bf8131e8f330eda9f3135e7daccfffdb
Igor <kovil22@mail.ru>
2024-04-18 19:31:41 +0300

    added goods

---
+++ cars/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from cars import views
+
+app_name = 'cars'
+
+urlpatterns = [
+    path('', views.catalog, name='index'),
+    path('product/', views.product, name='product'),
+]
