ae543dc4b9998e6a010dbf94d5e4c66e84e9fddc
Igor <kovil22@mail.ru>
2024-04-25 19:40:12 +0300

    cars filter

--- cars/views.py
+++ cars/views.py
@@ -7,11 +7,20 @@ from cars.models import Products
 def catalog(request, category_slug):
 
     page = request.GET.get('page', 1)
+    on_sale = request.GET.get('on_sale', None)
+    order_by_price = request.GET.get('order_by_price', None)
 
     if category_slug == 'all':
         cars = Products.objects.all()
     else:
         cars = get_list_or_404(Products.objects.filter(category__slug=category_slug))
+    
+    if on_sale:
+        cars = cars.filter(discount__gt=0)
+
+    if order_by_price and order_by_price != "default":
+        cars = cars.order_by(order_by_price)
+
 
     paginator = Paginator(cars, 6)
     current_page = paginator.page(int(page))
