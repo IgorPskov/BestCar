1ecf555654f42e173f3fa58c88dabe0188b93407
Igor <kovil22@mail.ru>
2024-04-29 12:10:38 +0300

    postgres and full text search

--- cars/utils.py
+++ cars/utils.py
@@ -1,17 +1,30 @@
+from turtle import Vec2D
 from django.db.models import Q
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 
 from cars.models import Products
 
 
 def q_search(query):
     
-    keywords = [word for word in query.split() if len(word) > 2]
+    if len(query) > 2:
 
-    q_objects = Q()
+        vector = SearchVector("category__name", "name", "description")
+        query = SearchQuery(query)
 
-    for token in keywords:
-        q_objects |= Q(description__icontains=token)
-        q_objects |= Q(name__icontains=token)
-        q_objects |= Q(category__name__icontains=token)
+        return Products.objects.annotate(rank=SearchRank(vector, query).order_by("-rank"))
+
+    else:
+        
+        return Products.objects.none()
+
+    # keywords = [word for word in query.split() if len(word) > 2]
+
+    # q_objects = Q()
+
+    # for token in keywords:
+    #     q_objects |= Q(description__icontains=token)
+    #     q_objects |= Q(name__icontains=token)
+    #     q_objects |= Q(category__name__icontains=token)
     
-    return Products.objects.filter(q_objects)
+    # return Products.objects.filter(q_objects)
