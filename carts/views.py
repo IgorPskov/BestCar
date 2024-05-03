b47d3f94b6152bc5f2fac621825e2ee8561054d7
Igor <kovil22@mail.ru>
2024-05-03 20:08:47 +0300

    car names in alerts

--- carts/views.py
+++ carts/views.py
@@ -15,13 +15,16 @@ def cart_add(request, product_slug):
     if request.user.is_authenticated:
         carts = Cart.objects.filter(user=request.user, product=product)
 
+        product_category = product.category
+        product_name = product.name
+
         if carts.exists():
             messages.warning(request, "Этот автомобиль уже добавлен в вашу корзину")
         else:
             Cart.objects.create(user=request.user, product = product)
-        
-            messages.success(request, "Автомобиль добавлен в корзину")
-
+    
+            success_message = f"Автомобиль {product_category} {product_name} добавлен в корзину"
+            messages.success(request, success_message)
 
     return redirect(request.META['HTTP_REFERER'])
     
@@ -30,9 +33,18 @@ def cart_change(request, product_slug):
     ...
 
 def cart_remove(request, cart_id):
-
     cart = Cart.objects.get(id=cart_id)
+    
+    # Получаем информацию о товаре из объекта корзины
+    product_category = cart.product.category
+    product_name = cart.product.name
+    
+    # Удаляем объект корзины
     cart.delete()
+
+    # Формируем сообщение об успешном удалении из корзины
+    success_message = f"Автомобиль {product_category} {product_name} удален из корзины"
+    messages.success(request, success_message)
 
     return redirect(request.META['HTTP_REFERER'])
 
@@ -42,6 +54,9 @@ def cart_remove(request, cart_id):
 def favorite_add(request):
     product_id = request.POST.get("product_id")    
     product = Products.objects.get(id=product_id)
+
+    product_category = product.category
+    product_name = product.name
 
     if request.user.is_authenticated:
         favorites = Favorite.objects.filter(user=request.user, product=product)
@@ -53,10 +68,13 @@ def favorite_add(request):
                 "warning_message": "Этот автомобиль уже добавлен к вам в избранное",
             }
         else:
+
             Favorite.objects.create(user=request.user, product=product)
+            success_message = f"Автомобиль {product_category} {product_name} добавлен в избранное"
+
             response_data = {
                 "already_in_favorite": False,
-                "success_message": "Автомобиль добавлен в избранное",
+                "success_message": success_message,
             }
     
     user_favorite = get_user_favorites(request)
@@ -72,6 +90,10 @@ def favorite_remove(request):
     favorite_id = request.POST.get("favorite_id")
 
     favorite = Favorite.objects.get(id=favorite_id)
+
+    product_category = favorite.product.category
+    product_name = favorite.product.name
+
     quantity = Favorite.objects.filter(user=request.user).count()
     favorite.delete()
 
@@ -79,8 +101,9 @@ def favorite_remove(request):
     favorite_items_html = render_to_string(
         "carts/includes/included_favorite.html", {"favorites": user_favorite}, request=request)
 
+    success_message = f"Автомобиль {product_category} {product_name} удален из избранного"
     response_data = {
-        "message": "Автомобиль удален из избранного",
+        "message": success_message,
         "favorite_items_html": favorite_items_html,
         "quantity_deleted": quantity, 
     }
