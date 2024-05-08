8762bd5a1e349bcb40c2aff7721779d3baaa07d7
Igor <kovil22@mail.ru>
2024-05-08 16:05:47 +0300

    my orders

--- orders/forms.py
+++ orders/forms.py
@@ -6,12 +6,34 @@ class CreateOrderForm(forms.Form):
     first_name = forms.CharField()
     last_name = forms.CharField()
     phone = forms.CharField()
-    requires_delivery = forms.ChoiceField()
+    requires_delivery = forms.ChoiceField(
+        choices=[
+            ("0", 'False'),
+            ("1", 'True'),
+        ],
+    )
     delivery_address = forms.CharField(required=False)
     delivery_price = forms.CharField(required=False)
-    requires_installment = forms.ChoiceField()
-    installment = forms.ChoiceField(required=False)
-    payment_on_get = forms.ChoiceField()
+    requires_installment = forms.ChoiceField(
+        choices=[
+            ("0", 'False'),
+            ("1", 'True'),
+        ],
+    )
+    installment = forms.ChoiceField(
+        choices=[
+            ("0"),
+            ("1"),
+            ("2"),
+            ("3"),
+        ],
+    )
+    payment_on_get = forms.ChoiceField(
+        choices=[
+            ("0", 'False'),
+            ("1", 'True'),
+        ],
+    )
 
-    
+
 
--- orders/views.py
+++ orders/views.py
@@ -1,34 +1,89 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.db import transaction
+from django.forms import ValidationError
+from django.shortcuts import redirect, render
 
 from carts.models import  Cart, CartQuerySet 
 from cars.utils import calculate_installment
 
+from orders.forms import CreateOrderForm
+from orders.models import Order, OrderItem
+
 
 def create_order(request):
 
-    #Рассрочка
-    if request.user.is_authenticated:
-        # Получаем объекты корзины текущего пользователя
-        user_carts = Cart.objects.filter(user=request.user)
+    if request.method == 'POST':
+        form = CreateOrderForm(request.POST)
+        if form.is_valid():
+            try:
+                with transaction.atomic():
+                    user = request.user
+                    cart_items = Cart.objects.filter(user=user)
 
-        # Вызываем метод total_price для получения общей суммы
-        total_price = user_carts.total_price()
-        total_price = float(total_price)
 
-        installment_24 = calculate_installment(total_price, 24)
-        installment_36 = calculate_installment(total_price, 36)
-        installment_60 = calculate_installment(total_price, 60)
+                    if cart_items.exists():
+                        #Создать заказ
+                        order = Order.objects.create(
+                            user=user,
+                            requires_delivery=form.cleaned_data['requires_delivery'],
+                            delivery_address=form.cleaned_data['delivery_address'],
+                            delivery_price=form.cleaned_data['delivery_price'],
+                            requires_installment=form.cleaned_data['requires_installment'],
+                            installment=form.cleaned_data['installment'],
+                            payment_on_get=form.cleaned_data['payment_on_get'],
+                        )
 
-        context = {
-            'installment_24': installment_24,
-            'installment_36': installment_36,
-            'installment_60': installment_60,
-        }
+                        #Создать заказанные авто
+                        for cart_item in cart_items:
+                            product = cart_item.product
+                            name = cart_item.product.name
+                            price = cart_item.product.sell_price()
+
+                            OrderItem.objects.create(
+                                order=order,
+                                product=product,
+                                name=name,
+                                price=price,
+                            )
+                            product.save()
+
+                        #Очистить корзину пользователя после создания заказа
+                        cart_items.delete()
+
+                        messages.success(request, 'Заказ оформлен!')
+                        return redirect('user:profile')
+            except ValidationError as e:
+                #messages.success(request, str(e))
+                return redirect('cart:order')
+                        
+
     else:
-        # Если пользователь не аутентифицирован, предоставьте пустой контекст
-        context = {}
+        initial = {
+            'first_name': request.user.first_name,
+            'last_name': request.user.last_name,
+        }
+
+        form = CreateOrderForm(initial=initial)
+
+    
+    context = {
+        'title': 'BestCar - Оформление заказа',
+        'form': form,
+        # 'installment_24': installment_24,
+        # 'installment_36': installment_36,
+        # 'installment_60': installment_60,
+    }
     
     return render(request, 'orders/create_order.html', context=context)
 
 
+#Рассрочка
+# user_carts = Cart.objects.filter(user=request.user)
 
+# # Вызываем метод total_price для получения общей суммы
+# total_price = user_carts.total_price()
+# total_price = float(total_price)
+
+# installment_24 = calculate_installment(total_price, 24)
+# installment_36 = calculate_installment(total_price, 36)
+# installment_60 = calculate_installment(total_price, 60)
